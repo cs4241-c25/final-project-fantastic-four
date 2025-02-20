7fe0179b3865895445ebcb9b93947d11e81f5980
RodrigoMarce <marcenarorodrigo@gmail.com>
2025-02-20 09:10:36 -0500

    add list of events

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 import { signIn, signOut, useSession } from "next-auth/react"
-import {Button} from 'react-bootstrap'
+import {Button, ListGroup} from 'react-bootstrap'
+import {useEffect, useState} from "react";
+import Event from '../lib/event';
 
 export default function Home() {
+  const [events, setEvents] = useState<Event[]>([])
+
   const { status, data } = useSession({
     required: true,
     onUnauthenticated() {
@@ -10,9 +14,20 @@ export default function Home() {
     },
   })
 
-  if (status === "loading") {
-    return 
+  async function getEvents() {
+    const response = await fetch('api/getEvents');
+    const data = await response.json();
+    setEvents(data);
   }
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  if (status === "loading") {
+    return
+  }
+
   return (
     <>
       <Button onClick={() => signOut()}>
@@ -20,6 +35,15 @@ export default function Home() {
         <br/>
         {data.user!.name}
       </Button>
+      <ListGroup>
+        {events.map((event) => (
+            <ListGroup.Item key={event._id.toString()}>
+              <h2>{event.name}</h2>
+              <p>Date: {event.date}</p>
+              <p>{event.isActive? "Active" : "Not Active"}</p>
+            </ListGroup.Item>
+        ))}
+      </ListGroup>
     </>
   );
 }
