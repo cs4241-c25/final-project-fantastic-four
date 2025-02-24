fe6773865c752ca6431a366e9f6871cac8bd93f9
Vic <vjradina@wpi.edu>
2025-02-24 12:05:07 -0500

    most of checkin

---
+++ app/api/checkIn/route.ts
@@ -0,0 +1,11 @@
+import { MONGO_CLIENT_EVENTS, MongoClient } from "mongodb"
+import {NextResponse} from 'next/server';
+
+const client = new MongoClient(process.env.DB_URL!)
+
+export async function GET(){
+    await client.connect(); 
+    let guests = await client.db("fantastic-four").collection("guests").find().toArray();
+    return NextResponse.json(guests, { status: 200 });
+}
+    
--- components/checkIn.tsx
+++ components/checkIn.tsx
@@ -3,23 +3,47 @@ import React, {useState} from 'react'
 
 interface guestName{
     firstName: string; 
-    LastName: string; 
+    lastName: string; 
 }
 
 const CheckIn: React.FC = () => {
-    const [name, setName] = useState("")
-    const [timeChecked, setTimeChecked] = useState<string |null>(null)
+    const [name, setName] = useState<guestName>({firstName: "", lastName: ""})
+    const [nameExists, setNameExists] = useState<boolean | null>(null)
+    const [timeChecked, setTimeChecked] = useState("")
 
     const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) =>{
-        const inputVal = e.target.value; 
-        setName(inputVal)
+        const fullName = e.target.value.trim().split(" ")
+        const firstName = fullName[0] || ""
+        const lastName = fullName.length > 1? fullName.slice(1).join(" ") : ""
+        setName({ firstName, lastName})
         const now = new Date().toLocaleTimeString();
         setTimeChecked(now)
+
+        if(firstName && lastName){
+            checkName(firstName, lastName)
+
+        } else{
+            setNameExists(null)
+        }
     } 
+
+    const checkName = async(firstName: string, lastName: string) =>{
+        try{
+            const response = await fetch("")
+            const data = await response.json(); 
+            setNameExists(data.exists)
+
+        } catch (error) {
+            console.error("error checking name", error)
+            setNameExists(null)
+        }
+    }
+
+    
 
     return(
         <div>
-            <input id='searchName' type='text' value='name' onChange={handleChange}  ></input>
+            <input id='searchName' type='text' value={`${name.firstName} ${name.lastName}`.trim()} onChange={handleChange}  ></input>
         </div>
     )
 }
