773d3e174da0aced7e2b8e1c70820c6d20f08b29
Lev Giffune <levgiffune@gmail.com>
2025-02-25 14:31:52 -0500

    fix missing name field

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,20 +7,21 @@ import credentials from "next-auth/providers/credentials";
 declare module "next-auth" {
   interface Session extends DefaultSession{
     user: {
-      id: string
       role: string
-    }
+    } & DefaultSession['user']
   }
 
   interface User extends DefaultUser {
-    id: string,
+    id: string
+    name: string
     role: string
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT extends DefaultJWT {
-    id: string,
+    id: string
+    name: string
     role: string
   }
 }
