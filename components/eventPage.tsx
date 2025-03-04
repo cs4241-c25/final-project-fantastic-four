7e7ee3a5a6f72ff24510c556ba4f0c36c7fe96ef
Lev Giffune <levgiffune@gmail.com>
2025-03-04 13:42:46 -0500

    suspended component

---
+++ components/eventPage.tsx
@@ -0,0 +1,43 @@
+'use client'
+import Events from '@/components/eventList';
+import AddGuestForm from '@/components/addGuest';
+import Guest from '@/types/guest';
+import { useSearchParams } from 'next/navigation';
+import React from 'react';
+
+export default function Page(){
+ 
+    const eventID = useSearchParams().get('eventID')
+    
+    const [guests, setGuests] = React.useState<Guest[]>([])
+    async function getGuests() {
+        const response = await fetch('/api/getList', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({eventID}),
+        });
+        const data = await response.json();
+        setGuests(data);
+    }
+
+    React.useEffect(() => {
+        getGuests();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);
+
+    return (
+        <>
+            <div style={{ textAlign: "center", marginBottom: "20px" }}>
+                <AddGuestForm
+                    eventID = {eventID!}
+                    getGuests = {getGuests}
+                />
+            </div>
+            <Events
+                guests = {guests}
+            />
+        </>
+    );
+}
